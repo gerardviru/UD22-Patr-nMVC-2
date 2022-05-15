88d51f8e8fb80e84461c57b2aa638c56d4c7482c
Gerard Viñuela <gerardviru@gmail.com>
2022-05-15 16:40:00 +0200

    Rama Features UD22-MVC

---
+++ _Ej2/src/main/java/mainApp/App.java
@@ -0,0 +1,38 @@
+package mainApp;
+
+import Modelo.modeloClientes;
+import Modelo.modeloVideos;
+//import Vista.vistaC_cli;
+//import Vista.vistaC_vid;
+//import Vista.vistaR;
+//import Vista.vistaU;
+//import Vista.vistaUVid;
+
+public class App 
+{
+    public static void main( String[] args ) {
+    	
+    	modeloClientes modeloClientes = new modeloClientes();
+    	modeloClientes.querys();
+    	
+    	modeloVideos modeloVideos = new modeloVideos();
+    	modeloVideos.querys();
+    	
+//    	vistaC_cli vistaC_cli = new vistaC_cli();
+//    	vistaC_cli.vista();
+//    	
+//    	vistaC_vid vistaC_vid = new vistaC_vid();
+//    	vistaC_vid.vista();
+//    	
+//    	vistaR vistaR = new vistaR();
+//    	vistaR.vistaR();
+//    	
+//    	vistaU vistaU = new vistaU();
+//    	vistaU.vistaU();
+//    	
+//    	vistaUVid vistaUVid = new vistaUVid();
+//    	vistaUVid.vistaUVid();
+    	
+    	
+    }
+}
